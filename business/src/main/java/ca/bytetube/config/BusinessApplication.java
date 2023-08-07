cab444ba6e5635d280f8701a8a16ea49e642ed42
TPJE <jamespoju@gmail.com>
2023-08-06 21:03:01 -0400

    Added Business module, added chapter into MySQL, added test controller for business module.

---
+++ business/src/main/java/ca/bytetube/config/BusinessApplication.java
@@ -0,0 +1,26 @@
+package ca.bytetube.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("ca.bytetube")
+@MapperScan("ca.bytetube.server.mapper")
+
+public class BusinessApplication {
+    private static final Logger log = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("Start-up Successfully!");
+        log.info("Business add: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
---
+++ server/src/main/java/ca/bytetube/server/dto/ChapterDto.java
@@ -0,0 +1,10 @@
+package ca.bytetube.server.dto;
+
+import lombok.Data;
+
+@Data
+public class ChapterDto {
+    private String id;
+    private String courseId;
+    private String name;
+}
