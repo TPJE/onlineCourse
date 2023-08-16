06a652c11014af4385c72b8f3bbc573ae263a13e
TPJE <jamespoju@gmail.com>
2023-08-15 20:31:21 -0400

    Moved corsFilter to gateway module, added load balance (lb:// + registry center) into gateway application.properties.

--- gateway/src/main/java/ca/bytetube/gateway/GatewayApplication.java
+++ gateway/src/main/java/ca/bytetube/gateway/GatewayApplication.java
@@ -6,7 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,5 +23,24 @@ public class GatewayApplication {
         Environment env = app.run(args).getEnvironment();
         log.info("Start successfully!!");
         log.info("Gateway addr: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+    /**
+     * Config cross domain
+     */
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
--- server/src/main/java/ca/bytetube/server/config/CorsConfig.java
+++ server/src/main/java/ca/bytetube/server/config/CorsConfig.java
@@ -1,19 +1,23 @@
-package ca.bytetube.server.config;
+/**
+ * Move the server config (for cross domain solution) to gateway (GatewayApplication.class)
+ */
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig  implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .maxAge(3600); // No need to registry in 1 hr (send OPTIONS request)
-    }
-}
+//package ca.bytetube.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig  implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                .maxAge(3600); // No need to registry in 1 hr (send OPTIONS request)
+//    }
+//}
