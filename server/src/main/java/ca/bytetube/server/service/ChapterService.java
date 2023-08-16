4956b480e685eca7744117c0822a7621ce75b667
TPJE <jamespoju@gmail.com>
2023-08-15 21:11:39 -0400

    Added page helper into pom.xml, passed page helper test in server ChapterService.class.

--- server/src/main/java/ca/bytetube/server/service/ChapterService.java
+++ server/src/main/java/ca/bytetube/server/service/ChapterService.java
@@ -4,6 +4,7 @@ import ca.bytetube.server.domain.Chapter;
 import ca.bytetube.server.domain.ChapterExample;
 import ca.bytetube.server.dto.ChapterDto;
 import ca.bytetube.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,9 @@ public class ChapterService {
     private ChapterMapper chapterMapper;
 
     public List<ChapterDto> list() {
+        // Test if page helper works, try to get 1 record in 1 page.
+        PageHelper.startPage(1, 1);
+
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         List<ChapterDto> chapterDtoList = new ArrayList<>();
